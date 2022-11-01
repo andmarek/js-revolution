c0571db80faea971bff88d1de19fac99c8eeea20
Andrew <andwmarek@gmail.com>
2022-10-31 22:39:23 -0500

    got score working

--- game.js
+++ game.js
@@ -1,9 +1,33 @@
-// Keys
-window.addEventListener('keydown',this.check,false);
-
-
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
+
+let x = canvas.width / 2;
+let y = canvas.height;
+let dx = 0;
+let dy = -2;
+
+class Ball {
+    constructor(ctx, ex, why) {
+        this.ctx = ctx
+        this.ex = ex;
+        this.why = why;
+    }
+    drawBall() {
+        this.ctx.beginPath();
+        this.ctx.arc(this.ex, this.why, 10, 0, Math.PI * 2);  // circle, hence pi
+        this.ctx.fillStyle = "#0095DD";
+        this.ctx.fill();
+        this.ctx.closePath;
+    }
+}
+// Keys
+window.addEventListener('keydown',this.check,false);
+let int_score = 0;
+let left_target = new Ball(ctx, canvas.width / 2, canvas.height - 350);
+let right_target = new Ball(ctx, canvas.width / 3, canvas.height - 350);
+
+let left_moving = new Ball(ctx, canvas.width / 2, y);
+let right_moving = new Ball(ctx, canvas.width / 3, y);
 
 const buttons = {
     LEFT: 37,
@@ -17,52 +41,27 @@ const buttons = {
 // right = 39
 // down = 40 
 function check(e) {
-    alert(e.keyCode);
-}
+    if ((left_moving.why - left_target.why < 50) && (left_moving.why - left_target.why > 0)) {
 
-let x = canvas.width / 2;
-let y = canvas.height;
-let dx = 0;
-let dy = -2;
+        let score = document.getElementById("score_number");
+        score.innerHTML = '';
+        int_score += 1;
+        str_score = int_score + "";
 
-function drawBall(start_x, start_y) {
-    ctx.beginPath();
-    ctx.arc(start_x, start_y, 10, 0, Math.PI * 2);  // circle, hence pi
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath;
-}
+        console.log(score.textContent);
+        let updated_score_elem = document.createTextNode(str_score);
+        score.appendChild(updated_score_elem);
 
-class Ball {
-    constructor(ctx, ex, why) {
-        this.ctx = ctx
-        this.ex = ex;
-        this.why = why;
+        
+        console.log("FUCK")
     }
-
-    drawBall() {
-        this.ctx.beginPath();
-        this.ctx.arc(this.ex, this.why, 10, 0, Math.PI * 2);  // circle, hence pi
-        this.ctx.fillStyle = "#0095DD";
-        this.ctx.fill();
-        this.ctx.closePath;
-    }
-
-    
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    let left_target = new Ball(ctx, canvas.width / 2, canvas.height - 350);
-    let right_target = new Ball(ctx, canvas.width / 3, canvas.height - 350);
-
     left_target.drawBall();
     right_target.drawBall();
-
-    /* Moving arrows */
-    let left_moving = new Ball(ctx, canvas.width / 2, y);
-    let right_moving = new Ball(ctx, canvas.width / 3, y);
 
     left_moving.drawBall()
     right_moving.drawBall()
@@ -72,6 +71,13 @@ function draw() {
     }
 
     x += dx;
-    y += dy;
+    left_moving.why += dy;
+    right_moving.why += dy;
+    if (left_moving.why < 0) {
+        left_moving.why = canvas.height;
+    }
+    if (right_moving.why < 0) {
+        right_moving.why = canvas.height;
+    }
 }
 setInterval(draw, 10);
